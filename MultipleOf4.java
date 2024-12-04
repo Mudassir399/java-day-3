ac1de4186fa16a5ba371a0a15e83434e9fc12bce
Mudassir399 <mudassirm3456@gmail.com>
2024-12-04 12:42:01 +0530

    Add files via upload

---
+++ MultipleOf4.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class MultipleOf4
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if ((n&3)==0)
+        {
+            System.out.println("is multiple of 4");
+
+        }
+        else
+        {
+            System.out.println("is not multiple of 4"); 
+        }
+    }
+
+
+
+}
